dce857b91572dd3980a34de3a0b9fef089b63e9a
Rafaelmorales1982 <rafaelmorconcursos@gmail.com>
2024-11-28 11:43:18 -0300

    Ligação um time tem vários jogadores utilizando Array

--- POO-JAVA/maratona_java/src/academy/devdojo/maratonajava/javacore/associacao/dominio/Time1.java
+++ POO-JAVA/maratona_java/src/academy/devdojo/maratonajava/javacore/associacao/dominio/Time1.java
@@ -4,10 +4,47 @@ public class Time1 {
 	
 	private String nome;
 	private int anoFundado;
+	//associação muitos jogadores
+	private Jogador1[] jogadores;
 	
+	
+	//construtor padrão
 	public Time1(String nome, int anoFundado) {
-		this.nome = nome;
+		this.nome = nome;	
 		this.anoFundado = anoFundado;
+		
+		
+		
+	}
+	
+	//construtor com sobrecarga
+	public Time1(String nome, int anoFundado, Jogador1[] jogadores) {
+		this.nome = nome;	
+		this.anoFundado = anoFundado;
+		this.jogadores = jogadores;
+		
+		
+	}
+	
+	public void imprime() {
+		System.out.println("Nome Time .: "+this.nome+ " Foi fundado em .:"+this.anoFundado);
+		if(jogadores == null) {
+			return;
+		}
+		else {
+			for (Jogador1 jogador1 : jogadores) {
+				System.out.println("Jogador .:" +jogador1.getNome()+" Idade .:"+jogador1.getIdade());
+			}
+			
+		}
+	}
+	
+	public void setJogadores(Jogador1[] jogadores) {
+		this.jogadores = jogadores;
+	}
+	
+	public Jogador1[] getJogadores() {
+		return this.jogadores;
 	}
 	
 	public void setNome(String nome) {
--- POO-JAVA/maratona_java/src/academy/devdojo/maratonajava/javacore/associacao/test/JogadorTesteTime.java
+++ POO-JAVA/maratona_java/src/academy/devdojo/maratonajava/javacore/associacao/test/JogadorTesteTime.java
@@ -7,10 +7,21 @@ public class JogadorTesteTime {
 
 	public static void main(String[] args) {
 		Jogador1 jogador1 = new Jogador1("Rafael Morales",42);
+		Jogador1 jogador2 = new Jogador1("Morales",40);
 		Time1 time1 = new Time1("Corinthians", 1910);
-		
+		Jogador1[] jogadores = {jogador1, jogador2};
+	
+	
 		jogador1.setTime(time1);
+		time1.setJogadores(jogadores);
+		
+		System.out.println(" Jogador");
 		jogador1.imprime();
+		
+		
+		
+		System.out.println("Time e seus jogadores");
+		time1.imprime();
 		
 
 	}
